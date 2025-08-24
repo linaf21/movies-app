b32f35c54cf95152c9434c6dbd4a779aa59c3fbc
linaf21 <linafranco31@gmail.com>
2025-08-24 17:00:47 -0500

    modify initial structure and create home

---
+++ core/actions/movies/now-playing.action.ts
@@ -0,0 +1,16 @@
+import { MovieDBMoviesResponse } from "../../../infrastructure/interfaces/moviedb-response";
+import { MovieMapper } from "../../../infrastructure/mappers/movie.mapper";
+import { movieApi } from "../../api/movie-api";
+
+export const nowPlayingAction = async () => {
+  try {
+    const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
+
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+
+    return movies;
+  } catch (error) {
+    console.log(error);
+    throw "Cannot load now playing movies";
+  }
+};
