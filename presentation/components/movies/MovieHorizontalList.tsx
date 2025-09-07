e6c8e507beaef25f2ac1683bb90f9417042a7820
linaf21 <linafranco31@gmail.com>
2025-09-07 16:59:01 -0500

    Refactor top-rated movies query to support pagination; update HomeScreen and MovieHorizontalList for infinite scrolling

--- presentation/components/movies/MovieHorizontalList.tsx
+++ presentation/components/movies/MovieHorizontalList.tsx
@@ -1,19 +1,47 @@
 import { IMovie } from "@/infrastructure/interfaces/movie.interface";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Text,
+  View,
+} from "react-native";
 import MoviePoster from "./MoviePoster";
 
 interface IMovieHorizontalListProps {
   movies: IMovie[];
   title?: string;
   className?: string;
+  loadNextPage?: () => void;
 }
 
 const MovieHorizontalList = ({
   movies,
   title,
   className,
+  loadNextPage,
 }: IMovieHorizontalListProps) => {
+  const isLoading = useRef(false);
+
+  useEffect(() => {
+    setTimeout(() => (isLoading.current = false), 200);
+  }, [movies]);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return;
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+    const isEndReached =
+      contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+
+    if (!isEndReached) return;
+
+    isLoading.current = true;
+
+    loadNextPage?.();
+  };
+
   return (
     <View className={className}>
       {title && <Text className="text-3xl font-bold px-4 mb-2">{title}</Text>}
@@ -24,8 +52,9 @@ const MovieHorizontalList = ({
         renderItem={({ item }) => (
           <MoviePoster id={item.id} posterPath={item.poster} smallPoster />
         )}
-        keyExtractor={(item) => `${item.id}`}
+        keyExtractor={(item, i) => `${item.id}-${i}`}
         horizontal
+        onScroll={onScroll}
       />
     </View>
   );
