5c9c59c103b2294183ef1f4da8580b416f44c25c
linaf21 <linafranco31@gmail.com>
2025-09-07 16:13:50 -0500

    Add MainSlideshow and MoviePoster components; update HomeScreen layout and loading state

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,20 +1,26 @@
-import { FlatList, Text, View } from "react-native";
+import MainSlideshow from "@/presentation/components/movies/MainSlideshow";
+import { ActivityIndicator, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import useMovies from "../../presentation/hooks/useMovies";
 
 const HomeScreen = () => {
   const { nowPlayingQuery } = useMovies();
 
+  const safeArea = useSafeAreaInsets();
+
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="justify-center items-center flex-1">
+        <ActivityIndicator color={"purple"} size={30} />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      {nowPlayingQuery.isLoading && <Text>Loading...</Text>}
-      {nowPlayingQuery.isError && <Text>Error loading movies</Text>}
-      {nowPlayingQuery.data && (
-        <FlatList
-          data={nowPlayingQuery.data}
-          renderItem={({ item }) => <Text>{item.title}</Text>}
-          keyExtractor={(item) => item.id.toString()}
-        />
-      )}
+    <View style={{ padding: safeArea.top }}>
+      <Text className="text-3xl font-bold px-4 mb-2">Movies app</Text>
+
+      <MainSlideshow movies={nowPlayingQuery.data ?? []} />
     </View>
   );
 };
---
+++ presentation/components/movies/MoviePoster.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Image, Pressable } from "react-native";
+
+interface IMoviePosterProps {
+  id: number;
+  posterPath: string;
+  smallPoster?: boolean;
+  className?: string;
+}
+
+const MoviePoster = ({
+  id,
+  posterPath,
+  smallPoster,
+  className,
+}: IMoviePosterProps) => {
+  return (
+    <Pressable className={`active:opacity-90 px-2 ${className}`}>
+      <Image
+        source={{ uri: posterPath }}
+        className="shadow-lg rounded-2xl w-full h-full"
+        style={{
+          width: smallPoster ? 85 : 150,
+          height: smallPoster ? 130 : 250,
+        }}
+      />
+    </Pressable>
+  );
+};
+
+export default MoviePoster;
